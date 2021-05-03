4d3cfec1490597773abd222ec5e88dc37b56a896
Valerii Kovalchuk <kovvalole@gmail.com>
2021-05-03 13:39:22 +0300

    Add visit details page

--- src/pages/Visits/VisitsListItem.tsx
+++ src/pages/Visits/VisitsListItem.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 import { Visit } from '../../types';
 
 const VisitsListItem = ({ id, timestamp, name, removeVisit }: Visit & {
     removeVisit: (id: number) => void,
-}) => (
-    <li>
-        <span style={{ marginRight: '20px' }}>
-            {new Date(timestamp).toLocaleString()} | {name}
-        </span>
-        <button
-            type="button"
-            onClick={() => removeVisit(id)}
-        >
-            Remove
-        </button>
-    </li>
-);
+}) => {
+    const match = useRouteMatch();
+    return (
+        <li>
+            <Link to={`${match.url}/${id}`} style={{ marginRight: '20px' }}>
+                {new Date(timestamp).toLocaleString()} | {name}
+            </Link>
+            <button
+                type="button"
+                onClick={() => removeVisit(id)}
+            >
+                Remove
+            </button>
+        </li>
+    );
+}
 
 export default VisitsListItem;
--- src/pages/Visits/index.tsx
+++ src/pages/Visits/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
 import { Visit } from '../../types';
 import NameForm from '../../components/NameForm';
 import VisitsList from './VisitsList';
+import VisitDetails from './VisitDetails';
 
 const Visits = ({ visits, setVisits }: {
     visits: Visit[],
     setVisits: (visits: Visit[]) => void
 }) => {
+    const match = useRouteMatch();
     const getVisit = (name: string): Visit => {
         const timestamp = Date.now();
         return ({
@@ -17,19 +20,24 @@ const Visits = ({ visits, setVisits }: {
         });
     }
     return (
-        <div>
-            <NameForm
-                onSubmit={({ name }) => {
-                    setVisits([...visits, getVisit(name)]);
-                }}
-            />
-            <VisitsList
-                visits={visits}
-                removeVisit={id => {
-                    setVisits(visits.filter(visit => visit.id !== id))
-                }}
-            />
-        </div>
+        <Switch>
+            <Route path={`${match.path}/:visitId`}>
+                <VisitDetails visits={visits} />
+            </Route>
+            <Route path={match.path}>
+                <NameForm
+                    onSubmit={({ name }) => {
+                        setVisits([...visits, getVisit(name)]);
+                    }}
+                />
+                <VisitsList
+                    visits={visits}
+                    removeVisit={id => {
+                        setVisits(visits.filter(visit => visit.id !== id))
+                    }}
+                />
+            </Route>
+        </Switch>
     );
 }
 
